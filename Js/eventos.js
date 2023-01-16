cf99b6837ed0e6df1cea0971256fbfad9904cd1d
sankikiller <85462503+sankikiller@users.noreply.github.com>
2023-01-16 14:04:42 -0300

    agregue detección de ip en js

--- Js/eventos.js
+++ Js/eventos.js
@@ -1,4 +1,12 @@
-document.getElementsByClassName('fa-facebook-official').addEventListener('click',function(){
-    
-})
+/* obtener IP del visitante */
+async function getIpClient() {
+    try {
+      const response = await axios.get('https://api.ipify.org?format=json');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  getIpClient();
 
