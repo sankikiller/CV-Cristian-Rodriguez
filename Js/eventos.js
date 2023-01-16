05e0f843d685c05b84f33d7a1bed067c0ae33a5c
sankikiller <85462503+sankikiller@users.noreply.github.com>
2023-01-16 14:14:02 -0300

    cambie el código detector de Ips

--- Js/eventos.js
+++ Js/eventos.js
@@ -1,12 +1,11 @@
 /* obtener IP del visitante */
 async function getIpClient() {
-    try {
-      const response = await axios.get('https://api.ipify.org?format=json');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const response = await axios.get('https://api.ipify.org?format=json');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
   }
-  
-  getIpClient();
+}
+getIpClient();
 
